9584b55e0b758f8cfd63ce21bf49e3cf00ef366b
WhiteNoise0000 <white.noise.0000@gmail.com>
2024-11-30 15:45:17 +0900

    CSP対策有効化を保留 #7

--- src/main/java/jp/whitenoise/JfTestApplication.java
+++ src/main/java/jp/whitenoise/JfTestApplication.java
@@ -1,13 +1,10 @@
 package jp.whitenoise;
 
-import java.util.UUID;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.vaadin.flow.server.VaadinServiceInitListener;
-import com.vaadin.flow.server.VaadinSession;
 
 @SpringBootApplication
 public class JfTestApplication {
@@ -26,13 +23,15 @@ public class JfTestApplication {
                 response.getDocument().head().parent().attr("lang", "ja");
 
                 // Prodction環境の場合、Strict CSP有効
-                if (response.getVaadinRequest().getService().getDeploymentConfiguration().isProductionMode()) {
-                    String nonce = UUID.randomUUID().toString();
-                    VaadinSession.getCurrent().setAttribute("csp-nonce", nonce);
-                    response.getVaadinResponse().setHeader("Content-Security-Policy",
-                            "script-src 'nonce-" + nonce + "'");
-                    response.getDocument().getElementsByTag("script").attr("nonce", nonce);
-                }
+                // TODO 有効化しても想定通り動かず様子見
+                // https://vaadin.com/forum/t/vaadin-flow-content-security-policy-csp/167899
+                //                if (response.getVaadinRequest().getService().getDeploymentConfiguration().isProductionMode()) {
+                //                    String nonce = UUID.randomUUID().toString();
+                //                    VaadinSession.getCurrent().setAttribute("csp-nonce", nonce);
+                //                    response.getVaadinResponse().setHeader("Content-Security-Policy",
+                //                            "script-src 'self' 'unsafe-eval' 'nonce-" + nonce + "';");
+                //                    response.getDocument().getElementsByTag("script").attr("nonce", nonce);
+                //                }
             });
         };
     }
