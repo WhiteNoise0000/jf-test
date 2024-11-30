481898abc406ea08d4136055ab2817dcca831777
WhiteNoise0000 <white.noise.0000@gmail.com>
2024-11-30 14:40:46 +0900

    XSS対策（CSP設定追加） #7

--- src/main/java/jp/whitenoise/JfTestApplication.java
+++ src/main/java/jp/whitenoise/JfTestApplication.java
@@ -1,7 +1,13 @@
 package jp.whitenoise;
 
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.vaadin.flow.server.VaadinServiceInitListener;
+import com.vaadin.flow.server.VaadinSession;
 
 @SpringBootApplication
 public class JfTestApplication {
@@ -12,4 +18,22 @@ public class JfTestApplication {
         SpringApplication.run(JfTestApplication.class, args);
     }
 
+    @Bean
+    VaadinServiceInitListener vaadinServiceInitListener() {
+        return event -> {
+            event.addIndexHtmlRequestListener(response -> {
+                // htmlタグのlang属性を"ja"に固定
+                response.getDocument().head().parent().attr("lang", "ja");
+
+                // Prodction環境の場合、Strict CSP有効
+                if (response.getVaadinRequest().getService().getDeploymentConfiguration().isProductionMode()) {
+                    String nonce = UUID.randomUUID().toString();
+                    VaadinSession.getCurrent().setAttribute("csp-nonce", nonce);
+                    response.getVaadinResponse().setHeader("Content-Security-Policy",
+                            "script-src 'nonce-" + nonce + "'");
+                    response.getDocument().getElementsByTag("script").attr("nonce", nonce);
+                }
+            });
+        };
+    }
 }
