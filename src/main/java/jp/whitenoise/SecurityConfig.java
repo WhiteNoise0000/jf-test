0b9b88c0bd730714ee12257a850dd0eb2df1ffc4
WhiteNoise0000 <white.noise.0000@gmail.com>
2024-12-06 20:42:25 +0900

    DB登録・更新ユーザ名を自動設定 #5

--- src/main/java/jp/whitenoise/SecurityConfig.java
+++ src/main/java/jp/whitenoise/SecurityConfig.java
@@ -1,16 +1,22 @@
 package jp.whitenoise;
 
+import java.util.Optional;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
 
+import jp.whitenoise.common.auth.User;
 import jp.whitenoise.common.auth.ui.LoginPage;
 
 @EnableWebSecurity
@@ -40,5 +46,22 @@ public class SecurityConfig extends VaadinWebSecurity {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * DB登録更新ユーザ名取得.
+     * 
+     * @return ログイン中ユーザ名
+     */
+    @Bean
+    AuditorAware<String> auditorAware() {
+        return () -> {
+            // AuthServiceを利用するとDIループとなるため、同一ロジックで取得
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            if (auth == null || !auth.isAuthenticated()) {
+                return Optional.empty();
+            }
+            return Optional.of(((User) auth.getPrincipal()).getUsername());
+        };
     }
 }
