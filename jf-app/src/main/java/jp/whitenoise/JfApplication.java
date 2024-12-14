3f11a65b014d22c49e2bcd2ba941bf638e8fc3ff
WhiteNoise0000 <white.noise.0000@gmail.com>
2024-12-14 20:28:15 +0900

    Hillaを除外しStrict CSPを改めて有効化 #7

--- jf-app/src/main/java/jp/whitenoise/JfApplication.java
+++ jf-app/src/main/java/jp/whitenoise/JfApplication.java
@@ -1,10 +1,13 @@
 package jp.whitenoise;
 
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.vaadin.flow.server.VaadinServiceInitListener;
+import com.vaadin.flow.server.VaadinSession;
 
 @SpringBootApplication
 public class JfApplication {
@@ -23,15 +26,18 @@ public class JfApplication {
                 response.getDocument().head().parent().attr("lang", "ja");
 
                 // Prodction環境の場合、Strict CSP有効
-                // TODO 有効化しても想定通り動かず様子見
-                // https://vaadin.com/forum/t/vaadin-flow-content-security-policy-csp/167899
-                //                if (response.getVaadinRequest().getService().getDeploymentConfiguration().isProductionMode()) {
-                //                    String nonce = UUID.randomUUID().toString();
-                //                    VaadinSession.getCurrent().setAttribute("csp-nonce", nonce);
-                //                    response.getVaadinResponse().setHeader("Content-Security-Policy",
-                //                            "script-src 'self' 'unsafe-eval' 'nonce-" + nonce + "';");
-                //                    response.getDocument().getElementsByTag("script").attr("nonce", nonce);
-                //                }
+                // ※有効化のためにHillaを除外（24.5.8時点）
+                // @see https://vaadin.com/forum/t/vaadin-flow-content-security-policy-csp/167899
+                if (response.getVaadinRequest().getService().getDeploymentConfiguration().isProductionMode()) {
+                    String nonce = UUID.randomUUID().toString();
+                    VaadinSession.getCurrent().setAttribute("csp-nonce", nonce);
+                    response.getVaadinResponse().setHeader("Content-Security-Policy",
+                            "script-src 'self' 'unsafe-eval' 'nonce-" + nonce + "';" +
+                                    "script-src 'self' ajax.cloudflare.com;" +
+                                    "script-src static.cloudflareinsights.com;" +
+                                    "connect-src cloudflareinsights.com;");
+                    response.getDocument().getElementsByTag("script").attr("nonce", nonce);
+                }
             });
         };
     }
